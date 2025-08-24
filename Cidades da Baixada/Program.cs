66f2c64e9a4e2454f2cee8c2a6a938e44b6e020a
Jessica Melo <jpanndora@gmail.com>
2025-08-24 00:39:31 -0300

    Termino da atividade

---
+++ Cidades da Baixada/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Berioga");
+Console.WriteLine("Cubataõ");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Santos");
+Console.WriteLine("São Vicente");
+;
+
